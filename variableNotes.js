73e3f1557db6b9565fd1010358534bda09523d7c
Babb <26BabbJ@badinhs.org>
2025-01-16 13:40:36 -0500

    math

--- variableNotes.js
+++ variableNotes.js
@@ -1,2 +1,55 @@
-// hi jon in the future.
-// this is jon from the past.
+// // hi jon in the future.
+// // this is jon from the past.
+
+// // declare a variable
+// let myName = "Jon";
+// console.log(myName);
+
+// //different way
+// const e = 2.71828;
+// console.log(e);
+
+// var pi = 3.1415;
+// console.log(pi);
+// pi = e;
+// console.log(pi);
+
+// // var and let are interchangable but const is a connstant variable that con't be changed
+
+// let total = 0;
+// let i = 0;
+// while (1 < 1001){
+//     total = total + i;
+//     i = i + 1
+// }
+// console.log(total);
+
+console.log(23+97);
+
+let sumOfSix = 10 +80 + 900 + 1000 + 10000 + 10;
+console.log(sumOfSix);
+
+const actual = 57;
+let prediction = actual - 13;
+let percentage = prediction / actual;
+let percentError = 1 - percentage;
+console.log(percentError);
+
+/* starts a multiline or block, comment */
+/*
+kdfjdf
+dkfjslfjd
+fkdfjdf
+sfkjds
+*/
+
+
+/*
+mathmatical operators in javaSprict
++ gives addition
+- gives subtraction
+* gives multiplaction
+/ gives division 
+** gives exponentiation
+% gives modulus */
+
