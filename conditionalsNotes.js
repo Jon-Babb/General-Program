1153f028d58a7613000ff08e568a826139abedc0
Babb <26BabbJ@badinhs.org>
2025-01-24 13:04:03 -0500

    ready

--- conditionalsNotes.js
+++ conditionalsNotes.js
@@ -1,1 +1,23 @@
 // these are notes on conditionals in programming ????
+
+/* If -> then
+look like math for comparisions
+*/
+/*let a = 5;
+console.log(5 < 3);
+console.log(5 > 3);
+console.log(a ==5);
+console.log(a == "5");
+console.log(a === "5");
+console.log(a != 3); */
+console.log("Badin" > "Ross");
+console.log("R" > "B");
+console.log("b" == "B");
+if (3 < 5){
+    console.log("3 is less than 5");
+}
+if(7 < 5){
+    console.log("7 is less than 5");
+} else {
+    console.log("7 is not less than 5");
+}
